7bc72cb8067e2fc6b92e4fd24f79e64b44e753dc
Manoj <manojlds@gmail.com>
2016-03-15 23:08:03 +0530

    Add example with variables

--- 1/main.tf
+++ 1/main.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region = "eu-west-1"
 }
+
 resource "aws_instance" "instance" {
   ami = "ami-1208b761"
   instance_type = "t1.micro"
---
+++ 2/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "ami" {}
+
+variable "instance_type" {
+  description = "Instance type for our dummy instance"
+}
